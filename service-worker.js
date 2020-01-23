ba7bdfdbe1bcb423c6e6e6da2f22d92c55249791
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-22 21:57:58 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jonny_Deates_Website_Wireframe/precache-manifest.d61b795fee3d610db336be7b82903379.js"
+  "/Jonny_Deates_Portfolio_Wireframe/precache-manifest.8603b788e472057b48eb5055ae7d4a04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jonny_Deates_Website_Wireframe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jonny_Deates_Portfolio_Wireframe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

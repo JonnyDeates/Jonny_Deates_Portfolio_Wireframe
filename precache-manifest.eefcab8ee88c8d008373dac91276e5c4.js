441aedcb8ab9467903556e10e5dea8a42b8e2767
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-01-22 21:43:36 -0700

    Updates

---
+++ precache-manifest.eefcab8ee88c8d008373dac91276e5c4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169e21e8934b0840931f1d9912edc6cd",
+    "url": "/Jonny_Deates_Website_Wireframe/index.html"
+  },
+  {
+    "revision": "8ddcb4abc525f751578c",
+    "url": "/Jonny_Deates_Website_Wireframe/static/css/main.b201e815.chunk.css"
+  },
+  {
+    "revision": "f14eec23d10aae85e684",
+    "url": "/Jonny_Deates_Website_Wireframe/static/js/2.befae699.chunk.js"
+  },
+  {
+    "revision": "8ddcb4abc525f751578c",
+    "url": "/Jonny_Deates_Website_Wireframe/static/js/main.28b63dab.chunk.js"
+  },
+  {
+    "revision": "5bf2d0089706f6f78365",
+    "url": "/Jonny_Deates_Website_Wireframe/static/js/runtime-main.19c28838.js"
+  },
+  {
+    "revision": "f51bbcf605f16dd8ec9599a0c5768dfb",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-floral-1.f51bbcf6.jpg"
+  },
+  {
+    "revision": "74a5f50d22f5adfca9796adec8ac322f",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-floral-2.74a5f50d.jpg"
+  },
+  {
+    "revision": "f5deafd7e91a609c40ae43c9c4fa26a1",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-floral-3.f5deafd7.jpg"
+  },
+  {
+    "revision": "e09021ab5d4a0655249ec41797297c24",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-floral2.e09021ab.gif"
+  },
+  {
+    "revision": "35bb3654815cabada954ae77697d7109",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-floral3.35bb3654.gif"
+  },
+  {
+    "revision": "bd042ccddcf330ab3def941d0caee33f",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-pattern-1.bd042ccd.jpg"
+  },
+  {
+    "revision": "6e974052b2fbbbbac449c32220a84821",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-pattern-2.6e974052.jpg"
+  },
+  {
+    "revision": "67dea3dea2b04fcad879cc1519a40c17",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-pattern-3.67dea3de.jpg"
+  },
+  {
+    "revision": "852e8b49855e5adb9a36f8a4ef0c881f",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-pattern1.852e8b49.gif"
+  },
+  {
+    "revision": "9066aeca9e26dd214d925c4939b3df6e",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-pattern2.9066aeca.gif"
+  },
+  {
+    "revision": "64bba37d245add9a6b04e5d1ff6c2edd",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/Chibi-pattern3.64bba37d.gif"
+  },
+  {
+    "revision": "17967f1b9dcd25b21df269be1affe80e",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/chibi-floral1.17967f1b.gif"
+  },
+  {
+    "revision": "087daa3720f894e2d5c3f9107d89bd87",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/minimalism.087daa37.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/python.067050f6.png"
+  },
+  {
+    "revision": "3d67b80e6004efc04b0f8487a578b333",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/reactjs.3d67b80e.png"
+  },
+  {
+    "revision": "4e96f1110cb872407e837d4fc1440f96",
+    "url": "/Jonny_Deates_Website_Wireframe/static/media/typescript.4e96f111.png"
+  }
+]);
